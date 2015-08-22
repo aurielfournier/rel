a216da4e0d31b2d80f70afe6ca55b4b6584194b8
Auriel Fournier <aurielfournier@gmail.com>
2015-08-22 14:05:41 -0500

    change from julian date to ordinal date

--- R/ordinal_date_con.R
+++ R/ordinal_date_con.R
@@ -1,24 +1,19 @@
-#' Turns Month/Day/Year into Julian Date
+#' Turns Month/Day/Year into Ordinal Date
 #'
-#' This function lets you convert regular dates to julian ones
+#' This function lets you convert regular dates to ordinal ones
 #' @param data.frame three columns (month/day/year)
-#' @keywords julian date
+#' @keywords ordinal date
 #' @export
 #' @examples
-#' jul_con()
+#' ordinal_date_con()
 
 
-
-jul_con <- function(x){
+ordinal_date_con <- function(x){ ## input = month/day/year.
   x <-   apply(x,2, as.numeric) ##this is to make sure they came in as a numeric not as a character or factor
+  x <- matrix(x, ncol=3)
   varmonth<-cbind(c(1:12),c(0,31,59,90,120,151,181,212,243,273,304,334))
-  juldates<-data.frame(date=as.numeric())
+  juldates<-data.frame(odate=as.numeric())
   for(i in 1:nrow(x)) {juldates[i,1] <- varmonth[which(x[i,1]==varmonth[,1]),2]+x[i,2]
-                                           
   }
-  juldates     
+  juldates$odate     
 }
-
-
-
-
