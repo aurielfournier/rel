3a636f8a42f2628180c02d355fe6c0ab42c58d49
Auriel Fournier <aurielfournier@gmail.com>
2016-01-22 16:41:57 -0600

    Update mround.R

--- R/mround.R
+++ R/mround.R
@@ -1,6 +1,6 @@
 #' Rounds numbers into categories by dec
 #'
-#' This function lets you convert regular dates to ordinal ones
+#' 
 #' @param a number that you want to round to a dec (decimal)
 #' @keywords rounding
 #' @export
