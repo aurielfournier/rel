dccbeb41fb308a15e3f5abb04e238cc2cefab631
aurielfournier <aurielfournier@gmail.com>
2017-11-13 16:45:17 -0600

    adding comments to script update

--- script_to_update_package.R
+++ script_to_update_package.R
@@ -2,6 +2,8 @@
 library(devtools)
 library(roxygen2)
 
+# Script to use on my computer for when the documentation needs to be updated and the package reinstalled from my computer
+
 # documentation
 
 setwd("~/rel")
